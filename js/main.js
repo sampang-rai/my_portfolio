c2bc3ed5d4949b7eda3e21b63a2b93d44d52672b
sampang-rai <rnk_rai@hotmail.com>
2025-02-07 02:53:39 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+ function toggleMenu() {
+    const menu = document.querySelector("#menu-icon");
+    const icon = document.querySelector(".navbar");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
